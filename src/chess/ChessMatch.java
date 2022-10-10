3443255d41d565492d99bc3fa947a23b4459785d
Vinicius Sousa <vinnysousa12@gmail.com>
2022-10-10 17:35:54 -0300

    Melhoria no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -22,10 +21,43 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		 	placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('b', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('c', 1, new King(board, Color.WHITE));
+	        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('f', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('g', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('a', 2, new Rook(board, Color.WHITE));
+	        placeNewPiece('b', 2, new Rook(board, Color.WHITE));
+	        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+	        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+	        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+	        placeNewPiece('f', 2, new Rook(board, Color.WHITE));
+	        placeNewPiece('g', 2, new Rook(board, Color.WHITE));
+	        placeNewPiece('h', 2, new Rook(board, Color.WHITE));
+
+	        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('c', 8, new King(board, Color.BLACK));
+	        placeNewPiece('d', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('f', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('g', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('a', 7, new Rook(board, Color.BLACK));
+	        placeNewPiece('b', 7, new Rook(board, Color.BLACK));
+	        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+	        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+	        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+	        placeNewPiece('f', 7, new Rook(board, Color.BLACK));
+	        placeNewPiece('g', 7, new Rook(board, Color.BLACK));
+	        placeNewPiece('h', 7, new Rook(board, Color.BLACK));
 	}
 }
